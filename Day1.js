3fd5c4460cba8f03d5b2b47bdb42aa6c3fdfd715
Ashfaq Khan <ashfaaq.alam@outlook.com>
2025-07-30 20:35:27 +0530

    Add function to find longest word in a string

---
+++ Day1.js
@@ -0,0 +1,34 @@
+//DAY-1:  WAF findLongestWord that takes a string as input and returns the longest word in the string.
+//        If there are multiple longest words, return the first one encountered.
+
+//	Constraints:
+//	  The input string may contain alphabetic characters, digits, spaces, and punctuations.
+//	  The input string is non-empty.
+//	  The input string may contain multiple words separated by spaces.
+
+//  Note:
+//	  If the input string is empty or contain only whitespace, the function returns false.
+//	  Function ignores leading & trailing whitespace when determining the longest word
+
+//Solution:
+
+const findLongestWord = (inputString) => {
+  const longestWord = inputString
+    .trim()
+    .replace(/[^\w\s\u2010\u2011-]/g, "")
+    .split(/\s+/);
+  return longestWord.sort((a, b) => a.length - b.length).at(-1);
+};
+
+// console.log(
+// findLongestWord(
+//   "The    quick‑brown  fox, surprisingly, leaped over   lazy‑dogs near the neighborhood!"
+//  )
+// );
+
+console.dir(
+  findLongestWord(
+    "The    quick‑brown  fox, surprisingly, leaped over   lazy‑dogs near the neighborhood!"
+  ),
+  { breakLength: 1, maxArrayLength: null }
+);
